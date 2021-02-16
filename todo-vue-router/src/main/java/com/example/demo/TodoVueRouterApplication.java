9fabad5c3c5ee7ae75f5b6d0eaa9288cc17ffbd3
eungsu <eungsu@me.com>
2021-02-16 18:09:29 +0900

    add CRUD method TocoController

--- todo-vue-router/src/main/java/com/example/demo/TodoVueRouterApplication.java
+++ todo-vue-router/src/main/java/com/example/demo/TodoVueRouterApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.example.demo.model.Todo;
+import com.example.demo.model.Todo.TodoStatus;
 import com.example.demo.repository.TodoRepository;
 
 @SpringBootApplication
@@ -14,6 +20,17 @@ public class TodoVueRouterApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TodoVueRouterApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner insertSampleTodo(TodoRepository todoRepository) {
+		return args -> {
+			todoRepository.save(Todo.builder().title("연습1").description("연습입니다.").dueDate(LocalDate.of(2021, 2, 21)).status(TodoStatus.REGISTERED).username("홍길동").build());
+			todoRepository.save(Todo.builder().title("연습2").description("연습입니다.").dueDate(LocalDate.of(2021, 2, 22)).status(TodoStatus.REGISTERED).username("홍길동").build());
+			todoRepository.save(Todo.builder().title("연습3").description("연습입니다.").dueDate(LocalDate.of(2021, 2, 23)).status(TodoStatus.REGISTERED).username("홍길동").build());
+			todoRepository.save(Todo.builder().title("연습4").description("연습입니다.").dueDate(LocalDate.of(2021, 2, 24)).status(TodoStatus.REGISTERED).username("홍길동").build());
+			todoRepository.save(Todo.builder().title("연습5").description("연습입니다.").dueDate(LocalDate.of(2021, 2, 25)).status(TodoStatus.REGISTERED).username("홍길동").build());
+		};
 	}
 
 }
--- todo-vue-router/src/main/java/com/example/demo/controller/TodoController.java
+++ todo-vue-router/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,17 +1,23 @@
 package com.example.demo.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.ResponseData;
+import com.example.demo.dto.ResponseData.ResponseDataStatus;
 import com.example.demo.model.Todo;
 import com.example.demo.repository.TodoRepository;
 
@@ -24,22 +30,59 @@ public class TodoController {
 	TodoRepository todoRepository;
 	
 	@GetMapping("/todos")
-	public ResponseEntity<Map<String, Object>> todos() {
-		Map<String, Object> result = new HashMap<>();
+	public ResponseEntity<ResponseData<Todo>> getTodos() {
+		ResponseData<Todo> responseData = new ResponseData<>();
 		
 		List<Todo> todos = todoRepository.findAll();
 		if (!todos.isEmpty()) {
-			result.put("status", "ok");
-			result.put("rows", todos.size());
-			result.put("items", todos);
+			responseData.setStatus(ResponseDataStatus.OK);
+			responseData.setRows(todos.size());
+			responseData.setItems(todos);
 		
-			return new ResponseEntity<>(result, HttpStatus.OK);
+			return new ResponseEntity<>(responseData, HttpStatus.OK);
 		} else {
-			result.put("status", "empty");
-			result.put("rows", 0);
-			result.put("items", List.of());
+			responseData.setStatus(ResponseDataStatus.EMPTY);
+			responseData.setItems(List.of());
 		
-			return new ResponseEntity<>(result, HttpStatus.OK);
+			return new ResponseEntity<>(responseData, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/todos/{id}")
+	public ResponseEntity<ResponseData<Todo>> getTodo(@PathVariable("id") long id) {
+		Optional<Todo> todo = todoRepository.findById(id);
+		ResponseData<Todo> responseData = new ResponseData<>();
+		if (todo.isPresent()) {
+			responseData.setStatus(ResponseDataStatus.OK);
+			responseData.setRows(1);
+			responseData.setItems(List.of(todo.get()));
+			return new ResponseEntity<>(responseData, HttpStatus.OK);
+		} else {
+			responseData.setStatus(ResponseDataStatus.NOTFOUND);
+			responseData.setMessage("아이디와 일치하는 작업정보를 찾을 수 없습니다.");
+			return new ResponseEntity<>(responseData, HttpStatus.OK);
+		}
+	}
+	
+	@PostMapping("/todos")
+	public ResponseEntity<Void> insertTodo(@RequestBody Todo todo) {
+		todoRepository.save(todo);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	@PutMapping("/todos/{id}")
+	public ResponseEntity<Void> updateTodo(@PathVariable("id") long id, @RequestBody Todo todo) {
+		Optional<Todo> savedTodo = todoRepository.findById(id);
+		ResponseData<Todo> responseData = new ResponseData<>();
+		if (savedTodo.isPresent()) {
+			responseData.setStatus(ResponseDataStatus.OK);
+			BeanUtils.copyProperties(todo, savedTodo.get());
+			todoRepository.save(savedTodo.get());
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			responseData.setStatus(ResponseDataStatus.NOTFOUND);
+			responseData.setMessage("아이디와 일치하는 작업정보를 찾을 수 없습니다.");
+			return new ResponseEntity<>(HttpStatus.OK);
 		}
 	}
 }
